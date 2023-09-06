fa64c1a91f5a049e960af1feb9449c9caedd2fb5
Kova / api <110469682+apilol@users.noreply.github.com>
2023-09-06 03:05:34 -0700

    🚀 Add examples directory

---
+++ examples/defender.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from threading import Thread
+
+from pydefender import PyDefender
+
+Defender = PyDefender(
+    debug=True,
+    modules=[
+        "AntiProcess",
+        "AntiVM",
+        "Miscellaneous",
+        "AntiDLL",
+        "AntiAnalysis",
+        "AntiDump"],
+    logs_path=Path.home() / "AppData/Roaming/PyDefender/logs/[Security].log",
+    webhook_url="%INSERT_WEBHOOK_URL&",
+    on_detect=[
+        "Report",
+        "Exit",
+        "Screenshot"],
+)
+
+if __name__ == "__main__":
+    DefenderThread = Thread(
+        name = "PyDefender Security", target=Defender.start
+    )
+    DefenderThread.start()
