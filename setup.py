96b262b22bd767b94be017aa13f3532fe6db1eb3
Faint <110469682+faintastic@users.noreply.github.com>
2024-10-04 21:30:28 -0700

    bump: Version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", encoding="utf-8") as read:
 
 setup(
     name="pythondefender",
-    version="1.0.3",
+    version="1.0.4",
     description="A simple, easy-to-use Python file protector.",
     packages=[
         "pydefender",
